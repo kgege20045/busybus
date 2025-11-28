c8273d4eacd1043be8f92c504dece60b74dc815e
kgege20045 <kgege20045@gmail.com>
2025-11-28 12:39:22 +0900

    update backend code

---
+++ busapi/ml_predict.py
@@ -0,0 +1,6 @@
+from django.conf import settings
+
+import joblib
+
+##모델 사용하여 예측하는 코드
+#입력 : routeid, select_time 형태
---
+++ busapi/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import run_training, predict_seat
+
+
+urlpatterns = [
+    path('predict-seat/', predict_seat, name='predict_seat'),
+    path('train/', run_training, name='run_training' ),
+
+]
+
