fe4dcf1ad4c58da7751e78230559576afed0db1c
kgege20045 <kgege20045@gmail.com>
2025-11-28 15:20:21 +0900

    add login

--- busapi/urls.py
+++ busapi/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import run_training, predict_seat
-
+from .views_auth import signup, login_view, logout_view, current_user
 
 urlpatterns = [
+    path('auth/signup/', signup),
+    path('auth/login/', login_view),
+    path('auth/logout/', logout_view),
+    path('auth/me/', current_user),
+
+
     path('predict-seat/', predict_seat, name='predict_seat'),
     path('train/', run_training, name='run_training' ),
 
 ]
-
